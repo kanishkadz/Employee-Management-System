7d376be9e6a503e549e2cfaf977efd25ebddb39d
Kanishka Anand <kanishkaadz@gmail.com>
2025-01-17 21:43:39 +0530

    Update Login.jsx

--- ems/src/components/Auth/Login.jsx
+++ ems/src/components/Auth/Login.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='flex items-center justify-center'>
+        <div className='border-2 border-red-600'>
+
+        </div>
+
+    </div>
   )
 }
 
