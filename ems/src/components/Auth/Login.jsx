a436d3391b3e6e23167e89f3b91e9b5c681bc43c
Kanishka Anand <kanishkaadz@gmail.com>
2025-01-17 21:53:36 +0530

    Update Login.jsx

--- ems/src/components/Auth/Login.jsx
+++ ems/src/components/Auth/Login.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const Login = () => {
   return (
     <div className='flex h-screen w-screen items-center justify-center'>
-        <div className='border-2 border-emerald-600 p-20'>
+        <div className='border-2 rounded-xl border-emerald-600 p-20'>
             <form className='flex flex-col items-center justify-center'>
                 <input className='text-black outline-none bg-transparent placeholder:text-white border-2 border-emerald-600 rounded-full py-3 px-5 text-xl' type="email" placeholder='Enter your email' />
                 <input className='text-black outline-none bg-transparent placeholder:text-white border-2 mt-3 border-emerald-600 rounded-full py-3 px-5 text-xl' type="password" placeholder='Enter password' />
-                <button className='text-white outline-none placeholder:text-white bg-emerald-600 rounded-full py-3 px-5 text-xl'>Log In</button>
+                <button className='text-white outline-none mt-5 placeholder:text-white bg-emerald-600 rounded-full py-3 px-5 text-xl'>Log In</button>
             </form>
         </div>
 
