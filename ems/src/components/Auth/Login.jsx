445c3acb1190ffb746f6d8cb8e264bbbb8b9c732
Kanishka Anand <kanishkaadz@gmail.com>
2025-01-17 21:44:03 +0530

    Update Login.jsx

--- ems/src/components/Auth/Login.jsx
+++ ems/src/components/Auth/Login.jsx
@@ -4,7 +4,9 @@ const Login = () => {
   return (
     <div className='flex items-center justify-center'>
         <div className='border-2 border-red-600'>
-
+            <form>
+                <input type="text" />
+            </form>
         </div>
 
     </div>
