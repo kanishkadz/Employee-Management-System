49db989edda46617115f011d4cd61fdadfa1a087
Kanishka Anand <kanishkaadz@gmail.com>
2025-01-17 22:01:29 +0530

    Update Login.jsx

--- ems/src/components/Auth/Login.jsx
+++ ems/src/components/Auth/Login.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
     const submitHandler = (e) => {
         e.preventDefault();
     }
