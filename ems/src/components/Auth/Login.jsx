38e11cf7ef135547c5013c8f8a9f185772512676
Kanishka Anand <kanishkaadz@gmail.com>
2025-01-17 22:09:44 +0530

    Update Login.jsx

--- ems/src/components/Auth/Login.jsx
+++ ems/src/components/Auth/Login.jsx
@@ -18,9 +18,9 @@ const Login = () => {
     <div className='flex h-screen w-screen items-center justify-center'>
         <div className='border-2 rounded-xl border-emerald-600 p-20'>
             <form onSubmit={(e) => {submitHandler(e)}} className='flex flex-col items-center justify-center'>
-                <input value={email} onChange={(e) => {setEmail(e.target.value)}} required className='outline-none bg-transparent placeholder:text-gray-400 border-2 border-emerald-600 rounded-full py-3 px-5 text-xl' type="email" placeholder='Enter your email' />
-                <input value={password} onChange={(e) => {setPassword(e.target.value)}} required className='outline-none bg-transparent placeholder:text-gray-400 border-2 mt-3 border-emerald-600 rounded-full py-3 px-5 text-xl' type="password" placeholder='Enter password' />
-                <button className='text-white outline-none mt-7 placeholder:text-white bg-emerald-600 rounded-full py-3 px-5 text-xl'>Log In</button>
+                <input value={email} onChange={(e) => {setEmail(e.target.value)}} required className='outline-none bg-transparent placeholder:text-gray-400 border-2 border-emerald-600 rounded-full py-2 px-6 text-lg' type="email" placeholder='Enter your email' />
+                <input value={password} onChange={(e) => {setPassword(e.target.value)}} required className='outline-none bg-transparent placeholder:text-gray-400 border-2 mt-3 border-emerald-600 rounded-full py-2 px-6 text-lg' type="password" placeholder='Enter password' />
+                <button className='text-white outline-none mt-7 placeholder:text-white bg-emerald-600 rounded-full hover:bg-emerald-700 py-2 px-8 text-lg w-full'>Log In</button>
             </form>
         </div>
 
