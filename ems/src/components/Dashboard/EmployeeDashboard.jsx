81c27e223b7b1894d27746b925fbc8a091ac9b68
Kanishka Anand <kanishkaadz@gmail.com>
2025-01-18 21:31:24 +0530

    2

--- ems/src/components/Dashboard/EmployeeDashboard.jsx
+++ ems/src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../Others/Header'
+import TaskListNumbers from '../Others/TaskListNumbers'
 
 const EmployeeDashboard = () => {
   return (
     <div>
         <div className='p-10 bg-[#1c1c1c] h-screen'>
             <Header />
+            <TaskListNumbers />
         </div>
     </div>
   )
--- ems/src/components/Others/TaskListNumbers.jsx
+++ ems/src/components/Others/TaskListNumbers.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const TaskListNumbers = () => {
   return (
     <div className='flex scren justify-between gap-5'>
-        <div className='h-40 w-1/2'>
+        <div className='h-40 w-1/2 bg-red-400'>
 
         </div>
     </div>
