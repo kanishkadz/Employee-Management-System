bac59f9d3d499f62dec8b814eee5bf813796dcc0
Kanishka Anand <kanishkaadz@gmail.com>
2025-01-18 21:52:10 +0530

    2

---
+++ ems/src/components/Dashboard/AdminDashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../Others/Header'
+
+const AdminDashboard = () => {
+  return (
+    <div>
+        <Header />
+    </div>
+  )
+}
+
+export default AdminDashboard
