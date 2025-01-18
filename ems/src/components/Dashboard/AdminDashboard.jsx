44dee0d6513fa1eab00ed1d932755a57f3e3a947
Kanishka Anand <kanishkaadz@gmail.com>
2025-01-18 21:58:07 +0530

    Update AdminDashboard.jsx

--- ems/src/components/Dashboard/AdminDashboard.jsx
+++ ems/src/components/Dashboard/AdminDashboard.jsx
@@ -8,16 +8,26 @@ const AdminDashboard = () => {
 
         <div>
             <form>
-                <h3>Task Title</h3>
-                <input type='text' placeholder='Make a UI Design' />
-                <h3>Description</h3>
-                <textarea name="" id=""></textarea>
-                <h3>Date</h3>
-                <input type="date"/>
-                <h3>Assign To</h3>
-                <input type="text" placeholder='Employee Name' />
-                <h3>Category</h3>
-                <input type="text" placeholder='Design, Development, etc' />
+                <div>
+                    <h3>Task Title</h3>
+                    <input type='text' placeholder='Make a UI Design' />
+                </div>
+                <div>
+                    <h3>Description</h3>
+                    <textarea name="" id=""></textarea>
+                </div>
+                <div>
+                    <h3>Date</h3>
+                    <input type="date"/>
+                </div>
+                <div>
+                    <h3>Assign To</h3>
+                    <input type="text" placeholder='Employee Name' />
+                </div>
+                <div>
+                    <h3>Category</h3>
+                    <input type="text" placeholder='Design, Development, etc' />
+                </div>
                 <button>Create Task</button>
             </form>
         </div>
