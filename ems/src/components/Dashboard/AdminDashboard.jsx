10753ea1d2df650b67fcbdcb75edace2c94ecde6
Kanishka Anand <kanishkaadz@gmail.com>
2025-01-18 21:56:19 +0530

    Update AdminDashboard.jsx

--- ems/src/components/Dashboard/AdminDashboard.jsx
+++ ems/src/components/Dashboard/AdminDashboard.jsx
@@ -5,6 +5,21 @@ const AdminDashboard = () => {
   return (
     <div className='h-screen w-full p-10'>
         <Header />
+
+        <div>
+            <form>
+                <h3>Task Title</h3>
+                <input type='text' placeholder='Make a UI Design' />
+                <h3>Description</h3>
+                <textarea name="" id=""></textarea>
+                <h3>Date</h3>
+                <input type="date"/>
+                <h3>Assign To</h3>
+                <input type="text" placeholder='Employee Name' />
+                <h3>Category</h3>
+                <input type="text" placeholder='Design, Development, etc' />
+            </form>
+        </div>
     </div>
   )
 }
