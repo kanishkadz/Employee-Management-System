6e38956f5788b9f6e2d776cc3de8599d5f5bf29f
Kanishka Anand <kanishkaadz@gmail.com>
2025-01-18 21:49:24 +0530

    Update TaskList.jsx

--- ems/src/components/TaskList/TaskList.jsx
+++ ems/src/components/TaskList/TaskList.jsx
@@ -11,6 +11,30 @@ const TaskList = () => {
             <h2 className='mt-5 text-2xl font-semibold'>Make a YouTube Video</h2>
             <p className='text-sm  mt-2'> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim eos vero rerum voluptatibus ex repudiandae. </p>
         </div>
+        <div className='flex-shrink-0 h-full w-[300px] p-5 bg-green-400 rounded-xl'>
+            <div className='flex justify-between items-center'>
+                <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
+                <h4 className='text-sm'>20 Feb 2024</h4>
+            </div>
+            <h2 className='mt-5 text-2xl font-semibold'>Make a YouTube Video</h2>
+            <p className='text-sm  mt-2'> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim eos vero rerum voluptatibus ex repudiandae. </p>
+        </div>
+        <div className='flex-shrink-0 h-full w-[300px] p-5 bg-blue-400 rounded-xl'>
+            <div className='flex justify-between items-center'>
+                <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
+                <h4 className='text-sm'>20 Feb 2024</h4>
+            </div>
+            <h2 className='mt-5 text-2xl font-semibold'>Make a YouTube Video</h2>
+            <p className='text-sm  mt-2'> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim eos vero rerum voluptatibus ex repudiandae. </p>
+        </div>
+        <div className='flex-shrink-0 h-full w-[300px] p-5 bg-yellow-400 rounded-xl'>
+            <div className='flex justify-between items-center'>
+                <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
+                <h4 className='text-sm'>20 Feb 2024</h4>
+            </div>
+            <h2 className='mt-5 text-2xl font-semibold'>Make a YouTube Video</h2>
+            <p className='text-sm  mt-2'> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim eos vero rerum voluptatibus ex repudiandae. </p>
+        </div>
     </div>
   )
 }
