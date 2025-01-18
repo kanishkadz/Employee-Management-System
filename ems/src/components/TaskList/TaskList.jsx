38aa64b996f1fba7994ffe535471ebefaf6592c9
Kanishka Anand <kanishkaadz@gmail.com>
2025-01-18 21:45:13 +0530

    2

--- ems/src/components/TaskList/TaskList.jsx
+++ ems/src/components/TaskList/TaskList.jsx
@@ -2,21 +2,12 @@ import React from 'react'
 
 const TaskList = () => {
   return (
-    <div className='h-[55%] flex items-center justify-start gap-5 flex-nowrap w-full py-5 bg-red-500 mt-10'>
-        <div className='flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl'>
-
-        </div>
-        <div className='flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl'>
-
-        </div>
-        <div className='flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl'>
-
-        </div>
-        <div className='flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl'>
-
-        </div>
-        <div className='flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl'>
-
+    <div id='tasklist' className='h-[55%] overflow-x-auto flex items-center justify-start gap-5 flex-nowrap w-full py-5 mt-10'>
+        <div className='flex-shrink-0 h-full w-[300px] p-5 bg-red-400 rounded-xl'>
+            <div className='flex justify-between items-center'>
+                <h3 className='bg-red-600 px-3 py-1 rounded'>High</h3>
+                <h4>20 Feb 2024</h4>
+            </div>
         </div>
     </div>
   )
