1689959d98baa7ab7c202fa204064c6231664a8a
Kanishka Anand <kanishkaadz@gmail.com>
2025-01-18 21:35:28 +0530

    Update TaskListNumbers.jsx

--- ems/src/components/Others/TaskListNumbers.jsx
+++ ems/src/components/Others/TaskListNumbers.jsx
@@ -7,15 +7,15 @@ const TaskListNumbers = () => {
             <h2 className='text-3xl font-semibold'>0</h2>
             <h3 className='text-xl font-medium'>New Task</h3>
         </div>
-        <div className='w-[45%] bg-red-400 py-6 px-9 rounded-xl'>
+        <div className='w-[45%] bg-blue-400 py-6 px-9 rounded-xl'>
             <h2 className='text-3xl font-semibold'>0</h2>
             <h3 className='text-xl font-medium'>New Task</h3>
         </div>
-        <div className='w-[45%] bg-red-400 py-6 px-9 rounded-xl'>
+        <div className='w-[45%] bg-green-400 py-6 px-9 rounded-xl'>
             <h2 className='text-3xl font-semibold'>0</h2>
             <h3 className='text-xl font-medium'>New Task</h3>
         </div>
-        <div className='w-[45%] bg-red-400 py-6 px-9 rounded-xl'>
+        <div className='w-[45%] bg-yellow-400 py-6 px-9 rounded-xl'>
             <h2 className='text-3xl font-semibold'>0</h2>
             <h3 className='text-xl font-medium'>New Task</h3>
         </div>
