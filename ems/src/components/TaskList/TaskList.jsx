4570e5fa1bbd3d496bf0afa9180bd2b05d102a40
Kanishka Anand <kanishkaadz@gmail.com>
2025-01-18 21:41:30 +0530

    Update TaskList.jsx

--- ems/src/components/TaskList/TaskList.jsx
+++ ems/src/components/TaskList/TaskList.jsx
@@ -2,8 +2,22 @@ import React from 'react'
 
 const TaskList = () => {
   return (
-    <div className='h-[55%] w-full bg-red-500 mt-10'>
+    <div className='h-[55%] flex items-center justify-start gap-5 flex-nowrap w-full py-5 bg-red-500 mt-10'>
+        <div className='flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl'>
 
+        </div>
+        <div className='flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl'>
+
+        </div>
+        <div className='flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl'>
+
+        </div>
+        <div className='flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl'>
+
+        </div>
+        <div className='flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl'>
+
+        </div>
     </div>
   )
 }
