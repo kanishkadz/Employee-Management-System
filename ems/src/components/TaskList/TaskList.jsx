2448242ed27bada51eba0ad25ccaf323ee7ce094
Kanishka Anand <kanishkaadz@gmail.com>
2025-01-18 21:46:44 +0530

    Update TaskList.jsx

--- ems/src/components/TaskList/TaskList.jsx
+++ ems/src/components/TaskList/TaskList.jsx
@@ -5,9 +5,10 @@ const TaskList = () => {
     <div id='tasklist' className='h-[55%] overflow-x-auto flex items-center justify-start gap-5 flex-nowrap w-full py-5 mt-10'>
         <div className='flex-shrink-0 h-full w-[300px] p-5 bg-red-400 rounded-xl'>
             <div className='flex justify-between items-center'>
-                <h3 className='bg-red-600 px-3 py-1 rounded'>High</h3>
-                <h4>20 Feb 2024</h4>
+                <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
+                <h4 className='text-sm'>20 Feb 2024</h4>
             </div>
+            <h2 className='mt-5 text-xl font-semibold'>Make a YouTube Video</h2>
         </div>
     </div>
   )
