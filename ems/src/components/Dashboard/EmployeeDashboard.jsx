808dfefd7a99b03423a330a5ca252a68c006eaa5
Kanishka Anand <kanishkaadz@gmail.com>
2025-01-18 21:36:59 +0530

    2

--- ems/src/components/Dashboard/EmployeeDashboard.jsx
+++ ems/src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../Others/Header'
 import TaskListNumbers from '../Others/TaskListNumbers'
+import TaskList from '../TaskList/TaskList'
 
 const EmployeeDashboard = () => {
   return (
@@ -8,6 +9,7 @@ const EmployeeDashboard = () => {
         <div className='p-10 bg-[#1c1c1c] h-screen'>
             <Header />
             <TaskListNumbers />
+            <TaskList />
         </div>
     </div>
   )
