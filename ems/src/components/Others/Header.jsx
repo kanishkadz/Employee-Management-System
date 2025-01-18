d82a7a13eb55daac75ab39d78f107ae996ab7987
Kanishka Anand <kanishkaadz@gmail.com>
2025-01-18 21:27:37 +0530

    Update Header.jsx

--- ems/src/components/Others/Header.jsx
+++ ems/src/components/Others/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='flex items-end justify-between'>
-        <h1>Hello <br/> Kanishka 👋</h1>
+        <h1 className='text-2xl'>Hello <br/> <span className='text-3xl'>Kanishka</span> 👋</h1>
         <button>Log Out</button>
     </div>
   )
