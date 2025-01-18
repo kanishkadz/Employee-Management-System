bec3c8c5cec5c78c90f1b90a2573eb64f7365837
Kanishka Anand <kanishkaadz@gmail.com>
2025-01-18 21:33:04 +0530

    Update TaskListNumbers.jsx

--- ems/src/components/Others/TaskListNumbers.jsx
+++ ems/src/components/Others/TaskListNumbers.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 const TaskListNumbers = () => {
   return (
-    <div className='flex scren justify-between gap-5'>
-        <div className='h-40 w-1/2 bg-red-400'>
-
+    <div className='mt-10  flex scren justify-between gap-5'>
+        <div className='h-40 w-[45] bg-red-400'>
+            <h2 className='text-3xl font-semibold'>0</h2>
+            <h3 className='text-xl font-medium'>New Task</h3>
         </div>
     </div>
   )
