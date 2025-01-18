b91b8472b7da5de41d04eaff3fb36ecfc8032c93
Kanishka Anand <kanishkaadz@gmail.com>
2025-01-18 21:37:36 +0530

    Update TaskList.jsx

--- ems/src/components/TaskList/TaskList.jsx
+++ ems/src/components/TaskList/TaskList.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const TaskList = () => {
   return (
-    <div className='h-[40%] w-full bg-red-500'>
+    <div className='h-[55%] w-full bg-red-500 mt-10'>
 
     </div>
   )
