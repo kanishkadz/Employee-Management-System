e8e7eb9d5e644d1a7c3b4821c515903c0b480d44
Kanishka Anand <kanishkaadz@gmail.com>
2025-01-20 22:01:24 +0530

    Update TaskList.jsx

--- ems/src/components/TaskList/TaskList.jsx
+++ ems/src/components/TaskList/TaskList.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import AcceptTask from './AcceptTask'
 import NewTask from './NewTask'
 import CompleteTask from './CompleteTask'
+import FailedTask from './FailedTask'
 
 const TaskList = ({data}) => {
   return (
@@ -9,14 +10,7 @@ const TaskList = ({data}) => {
         <AcceptTask /> 
         <NewTask />
         <CompleteTask />
-        <div className='flex-shrink-0 h-full w-[300px] p-5 bg-yellow-400 rounded-xl'>
-            <div className='flex justify-between items-center'>
-                <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
-                <h4 className='text-sm'>20 Feb 2024</h4>
-            </div>
-            <h2 className='mt-5 text-2xl font-semibold'>Make a YouTube Video</h2>
-            <p className='text-sm  mt-2'> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim eos vero rerum voluptatibus ex repudiandae. </p>
-        </div>
+        <FailedTask />
     </div>
   )
 }
