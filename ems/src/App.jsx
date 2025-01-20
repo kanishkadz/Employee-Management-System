3aec0e1ebe1cbc703803102bf27bdf4d49346719
Kanishka Anand <kanishkaadz@gmail.com>
2025-01-20 12:43:49 +0530

    Update App.jsx

--- ems/src/App.jsx
+++ ems/src/App.jsx
@@ -10,10 +10,18 @@ const App = () => {
   const [user, setUser] = useState(null) 
   const authData = useContext(AuthContext)
 
+  useEffect(() => {
+    if(authData){
+      const loggedInUser = localStorage.getItem("loggedInUser")
+    }
+  }, [authData])
+  
+
 
   const handleLogin = (email, password) => {
     if(email == 'admin@me.com' && password == '123'){
       setUser('admin')
+      localStorage.setItem('loggedInUser', JSON.stringify({role: 'admin'}))
     } else if(authData && authData.employees.find((e) => email == e.email && e.password == password)) {
       setUser('employee')
     } else {
