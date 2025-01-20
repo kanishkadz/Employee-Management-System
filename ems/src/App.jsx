e792431e846e22298cd18c39b6448f2bb8754b8f
Kanishka Anand <kanishkaadz@gmail.com>
2025-01-20 22:18:05 +0530

    Update App.jsx

--- ems/src/App.jsx
+++ ems/src/App.jsx
@@ -19,20 +19,20 @@ const App = () => {
       setUser(userData.role)
       setLoggedInUserData(userData.data)
     }
-  })
+  },[])
   
 
 
   const handleLogin = (email, password) => {
     if(email == 'admin@me.com' && password == '123'){
       setUser('admin')
-      localStorage.setItem('loggedInUser', JSON.stringify({role: 'admin'}))
+      localStorage.setItem('loggedInUser', JSON.stringify({role: 'admin', data: admin}))
     } else if(authData) {
       const employee = authData.employees.find((e) => email == e.email && e.password == password)
       if(employee){
         setUser('employee')
         setLoggedInUserData(employee)
-        localStorage.setItem('loggedInUser', JSON.stringify({role: 'employee'}))
+        localStorage.setItem('loggedInUser', JSON.stringify({role: 'employee', data: employee}))
       }
     } else {
       alert("Invalid Credentials")
