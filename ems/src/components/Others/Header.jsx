669ee3f1310d64aa5d305662663871cbfd5c0995
Kanishka Anand <kanishkaadz@gmail.com>
2025-01-21 21:29:12 +0530

    Update Header.jsx

--- ems/src/components/Others/Header.jsx
+++ ems/src/components/Others/Header.jsx
@@ -2,18 +2,22 @@ import React from 'react'
 
 const Header = ({data}) => {
 
-const [username, setUsername] = useState('')
+// const [username, setUsername] = useState('')
 
-if(!data){
-  setUsername('Admin')
-} else {
-  setUsername(data.fiirstName)
-}
+// if(!data){
+//   setUsername('Admin')
+// } else {
+//   setUsername(data.fiirstName)
+// }
+
+  const logOutUser = () => {
+    localStorage.set('loggedInUser', '')
+  }
 
   return (
     <div className='flex items-end justify-between'>
         <h1 className='text-2xl font-medium'>Hello <br/> <span className='text-3xl font-semibold'>{username} 👋</span></h1>
-        <button className='bg-red-600 text-lg font-medium text-white px-5 py-2 rounded-sm'>Log Out</button>
+        <button onClick={logOutUser} className='bg-red-600 text-lg font-medium text-white px-5 py-2 rounded-sm'>Log Out</button>
     </div>
   )
 }
