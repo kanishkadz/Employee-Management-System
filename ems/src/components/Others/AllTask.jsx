4466bec6f0bfb85e118f3ecf9f2fb04e7aaf068a
Kanishka Anand <kanishkaadz@gmail.com>
2025-01-23 22:27:33 +0530

    Update AllTask.jsx

--- ems/src/components/Others/AllTask.jsx
+++ ems/src/components/Others/AllTask.jsx
@@ -7,7 +7,7 @@ const AllTask = () => {
 
 
   return (
-    <div className='bg-[#1c1c1c] p-5 mt-5 rounded h-48'>
+    <div className='bg-[#1c1c1c] p-5 mt-5 rounded h-60'>
       <div className='bg-red-400 mb-2 py-2 px-4 flex justify-between rounded'>
         <h2 className='w-1/5'>Employee Name</h2>
         <h3 className='w-1/5'>New Task</h3>
