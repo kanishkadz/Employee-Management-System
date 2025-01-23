7709ba5e59b65e10effa7706f38fd76458ac9339
Kanishka Anand <kanishkaadz@gmail.com>
2025-01-23 22:25:15 +0530

    Update AllTask.jsx

--- ems/src/components/Others/AllTask.jsx
+++ ems/src/components/Others/AllTask.jsx
@@ -9,16 +9,16 @@ const AllTask = () => {
   return (
     <div className='bg-[#1c1c1c] p-5 mt-5 rounded h-48'>
       <div className='bg-red-400 mb-2 py-2 px-4 flex justify-between rounded'>
-        <h2 className='w-1/5 bg-red-600'>Employee Name</h2>
-        <h3 className='w-1/5 bg-red-600'>New Task</h3>
-        <h5 className='w-1/5 bg-red-600'>Active Task</h5>
-        <h5 className='w-1/5 bg-red-600'>Completed</h5>
-        <h5 className='w-1/5 bg-red-600'>Failed</h5>
+        <h2 className='w-1/5'>Employee Name</h2>
+        <h3 className='w-1/5'>New Task</h3>
+        <h5 className='w-1/5'>Active Task</h5>
+        <h5 className='w-1/5'>Completed</h5>
+        <h5 className='w-1/5'>Failed</h5>
       </div>
 
       <div className='h-[80%] overflow-auto'>
       {authData.employees.map(function(elem){
-        return <div className='bg-green-400 mb-2 py-2 px-4 flex justify-between rounded'>
+        return <div className='bg-emerald-500 mb-2 py-2 px-4 flex justify-between rounded'>
         <h2 className='w-1/5'>{elem.firstName}</h2>
         <h3 className='w-1/5 text-blue-600'>Task</h3>
         <h5 className='w-1/5 text-yellow-400'>Status</h5>
