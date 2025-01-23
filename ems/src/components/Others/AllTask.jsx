bda1b8735d22b586d6ddf90d68098251e63e6f1f
Kanishka Anand <kanishkaadz@gmail.com>
2025-01-23 22:21:39 +0530

    Update AllTask.jsx

--- ems/src/components/Others/AllTask.jsx
+++ ems/src/components/Others/AllTask.jsx
@@ -7,7 +7,7 @@ const AllTask = () => {
 
 
   return (
-    <div className='bg-[#1c1c1c] p-5 mt-5 rounded h-48 overflow-auto'>
+    <div className='bg-[#1c1c1c] p-5 mt-5 rounded h-48'>
       <div className='bg-red-400 mb-2 py-2 px-4 flex justify-between rounded'>
         <h2 className='w-1/5 bg-red-600'>Employee Name</h2>
         <h3 className='w-1/5 bg-red-600'>New Task</h3>
@@ -16,7 +16,7 @@ const AllTask = () => {
         <h5 className='w-1/5 bg-red-600'>Failed</h5>
       </div>
 
-      <div className='h-[80%]'>
+      <div className='h-[80%] overflow-auto'>
       {authData.employees.map(function(elem){
         return <div className='bg-green-400 mb-2 py-2 px-4 flex justify-between rounded'>
         <h2 className='w-1/5 bg-red-600'>{elem.firstName}</h2>
