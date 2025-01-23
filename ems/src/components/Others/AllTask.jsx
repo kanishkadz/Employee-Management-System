33d5747fd285e992976bfbf1b9c0deb5a15ec226
Kanishka Anand <kanishkaadz@gmail.com>
2025-01-23 22:07:50 +0530

    Update AllTask.jsx

--- ems/src/components/Others/AllTask.jsx
+++ ems/src/components/Others/AllTask.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../../context/AuthProvider'
 
 const AllTask = () => {
+
+  const authData = useContext(AuthContext)
+
+
   return (
     <div className='bg-[#1c1c1c] p-5 mt-5 rounded h-48 overflow-auto'>
       <div className='bg-red-400 mb-2 py-2 px-4 flex justify-between rounded'>
