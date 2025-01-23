45cf2d2de7eb54ee7e1b1a91f22acd29ef84601f
Kanishka Anand <kanishkaadz@gmail.com>
2025-01-23 22:30:24 +0530

    Update AllTask.jsx

--- ems/src/components/Others/AllTask.jsx
+++ ems/src/components/Others/AllTask.jsx
@@ -20,10 +20,10 @@ const AllTask = () => {
       {authData.employees.map(function(elem){
         return <div className='border-2 border-emerald-500 mb-2 py-2 px-4 flex justify-between rounded'>
         <h2 className='w-1/5'>{elem.firstName}</h2>
-        <h3 className='w-1/5 text-blue-600'>Task</h3>
-        <h5 className='w-1/5 text-yellow-400'>Status</h5>
-        <h5 className='w-1/5 text-white'></h5>
-        <h5 className='w-1/5 text-red-600'>Failed</h5>
+        <h3 className='w-1/5 text-blue-400'>{elem.taskCounts.newTask}</h3>
+        <h5 className='w-1/5 text-yellow-400'>{elem.taskCounts.active}</h5>
+        <h5 className='w-1/5 text-white'>{elem.taskCounts.completed}</h5>
+        <h5 className='w-1/5 text-red-600'>{elem.taskCounts.failed}</h5>
       </div>
       })}
       </div>
