5d61c4a0dea96e5de99339ac9393eb77f58efc18
Kanishka Anand <kanishkaadz@gmail.com>
2025-01-23 22:17:29 +0530

    Update AllTask.jsx

--- ems/src/components/Others/AllTask.jsx
+++ ems/src/components/Others/AllTask.jsx
@@ -8,11 +8,14 @@ const AllTask = () => {
 
   return (
     <div className='bg-[#1c1c1c] p-5 mt-5 rounded h-48 overflow-auto'>
-      {authData.employees.map(function(){
+      {authData.employees.map(function(elem){
+
+
         return <div className='bg-green-400 mb-2 py-2 px-4 flex justify-between rounded'>
-        <h2>Kanishka</h2>
-        <h3>Make a UI Design</h3>
-        <h5>Status</h5>
+        <h2 className='w-1/5 bg-red-600'>{elem.firstName}</h2>
+        <h3 className='w-1/5 bg-red-600'>Task</h3>
+        <h5 className='w-1/5 bg-red-600'>Status</h5>
+        <h5 className='w-1/5 bg-red-600'>Status</h5>
       </div>
       })}
       
