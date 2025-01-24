875c333d0e8c10bb38f2e3b038fc6d2645b41e2d
Kanishka Anand <kanishkaadz@gmail.com>
2025-01-24 21:29:30 +0530

    Update CreateTask.jsx

--- ems/src/components/Others/CreateTask.jsx
+++ ems/src/components/Others/CreateTask.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
 const CreateTask = () => {
+
+    const submitHandler = () => {
+        e.preventDefault()
+
+        
+    }
+
   return (
     <div className='p-5 bg-[#1c1c1c] mt-7 rounded'>
-            <form className='flex flex-wrap w-full items-start justify-between'>
+            <form onSubmit={(e)=>{
+                submitHandler(e)
+            }} className='flex flex-wrap w-full items-start justify-between'>
                 <div className='w-1/2'>
                     <div>
                         <h3 className='text-sm text-gray-300 mb-0.5'>Task Title</h3>
