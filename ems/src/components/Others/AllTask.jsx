c4f341ae23a8156c640512ea9bdad15a9694c137
Kanishka Anand <kanishkaadz@gmail.com>
2025-01-24 21:32:57 +0530

    5

--- ems/src/components/Others/AllTask.jsx
+++ ems/src/components/Others/AllTask.jsx
@@ -17,8 +17,8 @@ const AllTask = () => {
       </div>
 
       <div className='h-[80%] overflow-auto'>
-      {authData.employees.map(function(elem){
-        return <div className='border-2 border-emerald-500 mb-2 py-2 px-4 flex justify-between rounded'>
+      {authData.employees.map(function(elem, idx){
+        return <div key={idx} className='border-2 border-emerald-500 mb-2 py-2 px-4 flex justify-between rounded'>
         <h2 className='text-log font-medium w-1/5'>{elem.firstName}</h2>
         <h3 className='text-log font-medium w-1/5 text-blue-400'>{elem.taskCounts.newTask}</h3>
         <h5 className='text-log font-medium w-1/5 text-yellow-400'>{elem.taskCounts.active}</h5>
--- ems/src/components/Others/CreateTask.jsx
+++ ems/src/components/Others/CreateTask.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const CreateTask = () => {
 
-    const submitHandler = () => {
+    const submitHandler = (e) => {
         e.preventDefault()
 
-        
+
     }
 
   return (
