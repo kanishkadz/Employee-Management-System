2273d1dbd621ab9905f17549c029843ad6172fd8
Kanishka Anand <kanishkaadz@gmail.com>
2025-01-24 21:40:22 +0530

    Update CreateTask.jsx

--- ems/src/components/Others/CreateTask.jsx
+++ ems/src/components/Others/CreateTask.jsx
@@ -20,19 +20,19 @@ const CreateTask = () => {
                 <div className='w-1/2'>
                     <div>
                         <h3 className='text-sm text-gray-300 mb-0.5'>Task Title</h3>
-                        <input className='text-sm py-1 px-2 w-4/5 rouded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type='text' placeholder='Make a UI Design' />
+                        <input value={taskTitle} onChange={(e) => {setTaskTitle(e.target.value)}} className='text-sm py-1 px-2 w-4/5 rouded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type='text' placeholder='Make a UI Design' />
                     </div>
                     <div>
                         <h3 className='text-sm text-gray-300 mb-0.5'>Date</h3>
-                        <input className='text-sm py-1 px-2 w-4/5 rouded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type="date"/>
+                        <input value={taskDate} onChange={(e) => {setTaskDate(e.target.value)}} className='text-sm py-1 px-2 w-4/5 rouded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type="date"/>
                     </div>
                     <div>
                         <h3 className='text-sm text-gray-300 mb-0.5'>Assign To</h3>
-                        <input className='text-sm py-1 px-2 w-4/5 rouded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type="text" placeholder='Employee Name' />
+                        <input value={assignTo} onChange={(e) => {setAssignTo(e.target.value)}} className='text-sm py-1 px-2 w-4/5 rouded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type="text" placeholder='Employee Name' />
                     </div>
                     <div>
                         <h3 className='text-sm text-gray-300 mb-0.5'>Category</h3>
-                        <input className='text-sm py-1 px-2 w-4/5 rouded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type="text" placeholder='Design, Development, etc' />
+                        <input value={category} onChange={(e) => {setCategory(e.target.value)}} className='text-sm py-1 px-2 w-4/5 rouded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type="text" placeholder='Design, Development, etc' />
                     </div>
                 </div>                
                 <div className='w-2/5 flex flex-col items-start'>
