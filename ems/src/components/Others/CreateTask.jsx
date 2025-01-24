25b2bf418e9c78158aedac91ca9e32b0104e3bd6
Kanishka Anand <kanishkaadz@gmail.com>
2025-01-24 22:15:54 +0530

    Update CreateTask.jsx

--- ems/src/components/Others/CreateTask.jsx
+++ ems/src/components/Others/CreateTask.jsx
@@ -20,7 +20,7 @@ const CreateTask = () => {
 
         data.forEach(function(elem){
             if(assignTo == elem.firstName){
-
+                elem.tasks.push(newTask)
             }
         })
     }
