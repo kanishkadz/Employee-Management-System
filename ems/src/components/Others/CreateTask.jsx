1e7b10f2958c29e24673596599a4dfd4a54a8d6e
Kanishka Anand <kanishkaadz@gmail.com>
2025-01-24 21:41:53 +0530

    Update CreateTask.jsx

--- ems/src/components/Others/CreateTask.jsx
+++ ems/src/components/Others/CreateTask.jsx
@@ -37,7 +37,7 @@ const CreateTask = () => {
                 </div>                
                 <div className='w-2/5 flex flex-col items-start'>
                     <h3 className='text-sm text-gray-300 mb-0.5'>Description</h3>
-                    <textarea className='w-full h-44 text-sm py-2 px-4 rounded outline-none bg-transparent border-[1px] border-gray-400'  name="" id=""></textarea>
+                    <textarea value={taskDescription} onChange={(e) => {setTaskDescription(e.target.value)}} className='w-full h-44 text-sm py-2 px-4 rounded outline-none bg-transparent border-[1px] border-gray-400'  name="" id=""></textarea>
                     <button className='bg-emerald-500 py-3 hover:bg-emerald-600 px-5 rounded text-sm mt-4 w-full'>Create Task</button>
                 </div>                
             </form>
