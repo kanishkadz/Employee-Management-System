d7ab39ac3469bf4cfd28a2b9e15b1333ee940e6c
Kanishka Anand <kanishkaadz@gmail.com>
2025-01-24 21:55:16 +0530

    5

--- ems/src/components/Dashboard/EmployeeDashboard.jsx
+++ ems/src/components/Dashboard/EmployeeDashboard.jsx
@@ -3,13 +3,13 @@ import Header from '../Others/Header'
 import TaskListNumbers from '../Others/TaskListNumbers'
 import TaskList from '../TaskList/TaskList'
 
-const EmployeeDashboard = ({data}) => {
+const EmployeeDashboard = (props) => {
   return (
     <div>
         <div className='p-10 bg-[#1c1c1c] h-screen'>
-            <Header data={data} />
-            <TaskListNumbers data={data} />
-            <TaskList data={data} />
+            <Header data={props.data} />
+            <TaskListNumbers data={props.data} />
+            <TaskList data={props.data} />
         </div>
     </div>
   )
