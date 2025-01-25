9d9d87a1821d19a53a36c8a1160bfdb96749336c
Kanishka Anand <kanishkaadz@gmail.com>
2025-01-25 22:18:18 +0530

    Update EmployeeDashboard.jsx

--- ems/src/components/Dashboard/EmployeeDashboard.jsx
+++ ems/src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import Header from '../Others/Header'
-import TaskListNumbers from '../Others/TaskListNumbers'
+import Header from '../other/Header'
+import TaskListNumbers from '../other/TaskListNumbers'
 import TaskList from '../TaskList/TaskList'
 
 const EmployeeDashboard = (props) => {
+
   return (
-    <div>
-        <div className='p-10 bg-[#1c1c1c] h-screen'>
-            <Header changeUser={props.changeUser} data={props.data} />
-            <TaskListNumbers data={props.data} />
-            <TaskList data={props.data} />
-        </div>
+    <div className='p-10 bg-[#1C1C1C] h-screen'>
+        
+        <Header changeUser={props.changeUser} data={props.data}/>
+        <TaskListNumbers data={props.data} />
+        <TaskList data={props.data} />
     </div>
   )
 }
