836114c8662c6f2fcce1a4e30cd0485b5f5e881b
Kanishka Anand <kanishkaadz@gmail.com>
2025-01-25 22:23:05 +0530

    Update App.jsx

--- ems/src/App.jsx
+++ ems/src/App.jsx
@@ -1,49 +1,50 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Login from './components/Auth/Login'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
 import AdminDashboard from './components/Dashboard/AdminDashboard'
-import { setLocalStorage } from './utils/localStorage'
 import { AuthContext } from './context/AuthProvider'
 
 const App = () => {
 
-  const [user, setUser] = useState(null) 
+  const [user, setUser] = useState(null)
   const [loggedInUserData, setLoggedInUserData] = useState(null)
-  const [userData, setUserData] = useContext(AuthContext)
+  const [userData,SetUserData] = useContext(AuthContext)
 
-  useEffect(() => {
-    const loggedInUser = localStorage.setItem('loggeInUser', '')
-
+  useEffect(()=>{
+    const loggedInUser = localStorage.getItem('loggedInUser')
+    
     if(loggedInUser){
       const userData = JSON.parse(loggedInUser)
       setUser(userData.role)
       setLoggedInUserData(userData.data)
     }
+
   },[])
-  
 
 
   const handleLogin = (email, password) => {
-    if(email == 'admin@me.com' && password == '123'){
+    if (email == 'admin@me.com' && password == '123') {
       setUser('admin')
-      localStorage.setItem('loggedInUser', JSON.stringify({role: 'admin', data: admin}))
-    } else if(userData) {
+      localStorage.setItem('loggedInUser', JSON.stringify({ role: 'admin' }))
+    } else if (userData) {
       const employee = userData.find((e) => email == e.email && e.password == password)
-      if(employee){
+      if (employee) {
         setUser('employee')
         setLoggedInUserData(employee)
-        localStorage.setItem('loggedInUser', JSON.stringify({role: 'employee', data: employee}))
+        localStorage.setItem('loggedInUser', JSON.stringify({ role: 'employee',data:employee }))
       }
-    } else {
+    }
+    else {
       alert("Invalid Credentials")
     }
   }
 
 
+
   return (
     <>
-      {!user ? <Login handleLogin={handleLogin} /> : ' '}
-      {user == 'admin' ? <AdminDashboard changeUser = {setUser} /> : {user == 'employee' ? <EmployeeDashboard changeUser = {setUser} data={loggedInUserData} /> : null}}
+      {!user ? <Login handleLogin={handleLogin} /> : ''}
+      {user == 'admin' ? <AdminDashboard changeUser={setUser} /> : (user == 'employee' ? <EmployeeDashboard changeUser={setUser} data={loggedInUserData} /> : null) }
     </>
   )
 }
