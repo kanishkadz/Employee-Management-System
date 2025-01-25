4392fb47a9f693a50ef3248a8a8c47b408d37bb1
Kanishka Anand <kanishkaadz@gmail.com>
2025-01-25 22:22:07 +0530

    Update AuthProvider.jsx

--- ems/src/context/AuthProvider.jsx
+++ ems/src/context/AuthProvider.jsx
@@ -1,28 +1,28 @@
-import React from 'react'
+import React, { createContext, useEffect, useState } from 'react'
 import { getLocalStorage, setLocalStorage } from '../utils/localStorage'
-
 
 export const AuthContext = createContext()
 
-const AuthProvider = ({children}) => {
+const AuthProvider = ({ children }) => {
+    // localStorage.clear()
 
     const [userData, setUserData] = useState(null)
 
     useEffect(() => {
-      setLocalStorage()
-      const {employees} = getLocalStorage()
-      setUserData(employees)
+        setLocalStorage()
+        const {employees} = getLocalStorage()
+        setUserData(employees)
     }, [])
     
+    
 
-
-  return (
-    <div>
-        <AuthContext.provider value={[userData, setUserData]}>
-            {children}
-        </AuthContext.provider>
-    </div>
-  )
+    return (
+        <div>
+            <AuthContext.Provider value={[userData,setUserData]}>
+                {children}
+            </AuthContext.Provider>
+        </div>
+    )
 }
 
 export default AuthProvider
