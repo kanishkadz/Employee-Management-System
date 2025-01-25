a6a8f50ae5286e24f93194c902f834a37c0d6dbf
Kanishka Anand <kanishkaadz@gmail.com>
2025-01-25 22:02:19 +0530

    Update App.jsx

--- ems/src/App.jsx
+++ ems/src/App.jsx
@@ -28,7 +28,7 @@ const App = () => {
       setUser('admin')
       localStorage.setItem('loggedInUser', JSON.stringify({role: 'admin', data: admin}))
     } else if(authData) {
-      const employee = authData.find((e) => email == e.email && e.password == password)
+      const employee = userData.find((e) => email == e.email && e.password == password)
       if(employee){
         setUser('employee')
         setLoggedInUserData(employee)
