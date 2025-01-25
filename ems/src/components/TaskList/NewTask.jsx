ec28b3b26bfdd5d8b0a15f06ff4140d3163b4b61
Kanishka Anand <kanishkaadz@gmail.com>
2025-01-25 22:19:59 +0530

    Update NewTask.jsx

--- ems/src/components/TaskList/NewTask.jsx
+++ ems/src/components/TaskList/NewTask.jsx
@@ -8,9 +8,11 @@ const NewTask = ({data}) => {
                 <h4 className='text-sm'>{data.taskDate}</h4>
             </div>
             <h2 className='mt-5 text-2xl font-semibold'>{data.taskTitle}</h2>
-            <p className='text-sm  mt-2'>{data.Description}</p>
-            <div className='mt-4'>
-                <button>Accept Task</button>
+            <p className='text-sm mt-2'>
+                {data.taskDescription}
+            </p>
+            <div className='mt-6'>
+                <button className='bg-blue-500 rounded font-medium py-1 px-2 text-xs'>Accept Task</button>
             </div>
         </div>
     )
