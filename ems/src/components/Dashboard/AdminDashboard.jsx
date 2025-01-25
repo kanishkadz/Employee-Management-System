65442d639d56df3a6ebfca03291bfaedbf7ba7df
Kanishka Anand <kanishkaadz@gmail.com>
2025-01-25 22:18:01 +0530

    Update AdminDashboard.jsx

--- ems/src/components/Dashboard/AdminDashboard.jsx
+++ ems/src/components/Dashboard/AdminDashboard.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import Header from '../Others/Header'
-import CreateTask from '../Others/CreateTask'
-import AllTask from '../Others/AllTask'
+import Header from '../other/Header'
+import CreateTask from '../other/CreateTask'
+import AllTask from '../other/AllTask'
 
 const AdminDashboard = (props) => {
-  return (
-    <div className='h-screen w-full p-7'>
-        <Header changeUser = {props.changeUser} />
-        <CreateTask />
-        <AllTask />
-    </div>
-  )
+    return (
+        <div className='h-screen w-full p-7'>
+            <Header changeUser={props.changeUser} />
+            <CreateTask />
+            <AllTask />
+        </div>
+    )
 }
 
 export default AdminDashboard
